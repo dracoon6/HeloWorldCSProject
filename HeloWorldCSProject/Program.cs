2c697b745c2b608b24d5ffe0b6dc6b0a7a280538
dracoon6 <86674590+dracoon6@users.noreply.github.com>
2026-01-06 19:38:28 -0700

    init

--- HeloWorldCSProject/Program.cs
+++ HeloWorldCSProject/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ using System;
+ using System.Collections.Generic;
+ using System.IO;
+ using System.Linq;
+ using System.Net.Http;
+ using System.Threading;
+ using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
